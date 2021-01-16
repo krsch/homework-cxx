ddabd09c37d4e96b5c5573386584fe2028bcfbfc
Alexey Kreshchuk <akrsch@gmail.com>
2021-01-16 14:43:07 +0300

    update clang-format and clang-tidy

--- minmax/minmax.cpp
+++ minmax/minmax.cpp
@@ -1,5 +1,5 @@
 #include "minmax.hpp"
 
 std::pair<int, int> minmax(std::vector<int> const &input) {
-  return {input[0], input[0]};
+    return {input[0], input[0]};
 }
--- minmax/minmax_test.cpp
+++ minmax/minmax_test.cpp
@@ -3,10 +3,10 @@
 
 namespace doctest {
 template <> struct StringMaker<std::pair<int, int>> {
-  static String convert(const std::pair<int, int> &value) {
-    return String("{") + toString(value.first) + String(", ") +
-           toString(value.second) + String("}");
-  }
+    static String convert(const std::pair<int, int> &value) {
+        return String("{") + toString(value.first) + String(", ") +
+               toString(value.second) + String("}");
+    }
 };
 } // namespace doctest
 TEST_CASE("Single") { REQUIRE(minmax({1}) == std::pair{1, 1}); }
