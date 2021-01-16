b873968ddc0dda0b0cfab02a4fc7730284041232
Alexey Kreshchuk <akrsch@gmail.com>
2021-01-16 13:21:23 +0300

    large pqueue test

--- pqueue/pqueue_test.cpp
+++ pqueue/pqueue_test.cpp
@@ -81,3 +81,57 @@ TEST_CASE("extract_min tests 2") {
     CHECK(queue.extract_min() == "5");
     REQUIRE(queue.size() == 1);
 }
+
+TEST_CASE("large tests") {
+  pqueue queue;
+  std::vector<pqueue::handle> h;
+  h.reserve(1000000 - 500000);
+  for (int i = 4; i <= 499999; ++i) {
+    h.emplace_back(queue.insert(i, ""));
+  }
+
+  auto it_test = queue.insert(500000, "500000");
+
+  for (int i = 1000000; i > 500000; --i) {
+    h.emplace_back(queue.insert(i, ""));
+  }
+
+  auto i3 = queue.insert(3, "third");
+  auto i1 = queue.insert(1, "first");
+  auto i2 = queue.insert(2, "second");
+
+  REQUIRE(queue.size() == 1000000);
+  CHECK(queue.get(i1) == "first");
+  CHECK(queue.get(i2) == "second");
+  CHECK(queue.get(i3) == "third");
+  CHECK(queue.get_priority(i1) == 1);
+  CHECK(queue.get_priority(i2) == 2);
+  CHECK(queue.get_priority(i3) == 3);
+  queue.erase(i1);
+  REQUIRE(queue.size() == 999999);
+  CHECK(queue.get(i2) == "second");
+  CHECK(queue.get(i3) == "third");
+  CHECK(queue.get_priority(i2) == 2);
+  CHECK(queue.get_priority(i3) == 3);
+  queue.erase(i2);
+  REQUIRE(queue.size() == 999998);
+  CHECK(queue.get(i3) == "third");
+  CHECK(queue.get_priority(i3) == 3);
+
+  CHECK(queue.extract_min() == "third");
+
+  REQUIRE(queue.size() == 999997);
+  /* CHECK(queue.extract_min() == ""); */
+  /* REQUIRE(queue.size() == 999996); */
+
+  queue.erase(it_test);
+  /* CHECK(queue.size() == 999995); */
+
+  for (auto & hh : h)
+      queue.erase(hh);
+
+  /* for (int i = 0; i < 999995; ++i) { */
+  /*   CHECK(queue.extract_min() == ""); */
+  /* } */
+  REQUIRE(queue.size() == 0);
+}
