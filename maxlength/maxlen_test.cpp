4c8061e897283a2e5ccbf169b347d14105df73a8
wintsa <wintsa@gmail.com>
2021-01-19 13:50:38 +0300

    New test

--- maxlength/maxlen_test.cpp
+++ maxlength/maxlen_test.cpp
@@ -16,3 +16,5 @@ TEST_CASE("Long") { REQUIRE(maxlen({1, 1, 1, 1, 2, 3, 4}) == 4); }
 TEST_CASE("Double") { REQUIRE(maxlen({1, 2, 3, 4, 3, 2, 1, 2, 3}) == 4); }
 TEST_CASE("Same") { REQUIRE(maxlen({1, 1, 1, 1, 1}) == 1); }
 TEST_CASE("Empty") { REQUIRE(maxlen({}) == 0); }
+TEST_CASE("Shifted") { REQUIRE(maxlen({3, 4, 5, 0}) == 3); }
+TEST_CASE("Non-ordered") { REQUIRE(maxlen({3, 4, 2, 5}) == 2); }
